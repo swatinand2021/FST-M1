90453e6f16b4d0800119b4f9de970f9b0dbaf064
swatinand2021 <91046073+swatinand2021@users.noreply.github.com>
2021-09-27 16:34:23 +0530

    Add files via upload

---
+++ SQL/Activities/Activity1.sql
@@ -0,0 +1,14 @@
+	
+-- Create salesman table
+	
+CREATE TABLE salesman (
+	
+    salesman_id int,
+	
+    salesman_name varchar2(32),
+	
+    salesman_city varchar2(32),
+	
+    commission int
+	
+);
